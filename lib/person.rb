58f71dc22f461a5518fb7f13d765023858065b65
Abbas Parpia <aparpia03@gmail.com>
2020-02-21 23:03:11 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person 
+  def person=(name)
+    @name = name 
+  end 
+  
+  def person 
+    @name 
+  end  
+  
+  def job=(job)
+    @job = job
+  end 
+  
+  def job 
+    @job 
+  end   
+  
+end 
